d21d537e24ad9738cdc1eb3a6d1daf01d95d41ad
Ricardo Sebastián González Rivas <a01646105@tec.mx>
2026-05-31 01:29:32 -0600

    Actualizar DataMemory con mejores comentarios

--- Single Cycle RISC V/Ricardo Sebastian Gonzalez Rivas/DataMemory.v
+++ Single Cycle RISC V/Ricardo Sebastian Gonzalez Rivas/DataMemory.v
@@ -1,19 +1,24 @@
 module DataMemory(
-    // Inputs
+    // Entradas
     input clk,
-    input WE,
+    input WE,         // Señal de control en caso de requerir escritura
     input [31:0] A,
     input [31:0] WD,
     
-    // Outputs
+    // Salidas
     output [31:0] RD
 );
-    reg [31:0] RAM [63:0]; // Memoria pequeña para simulación
 
-    always @(posedge clk) begin
-        if (WE)
-            RAM[A[31:2]] <= WD;
-    end
+// Memoria pequeña para simulación
+reg [31:0] RAM [63:0]; 
 
-    assign RD = RAM[A[31:2]];
+// 
+always @(posedge clk) begin
+    if (WE)
+        RAM[A[31:2]] <= WD;
+end
+
+assign RD = RAM[A[31:2]];
+
+
 endmodule
