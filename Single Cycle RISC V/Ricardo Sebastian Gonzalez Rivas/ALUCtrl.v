10408ed91dbb61562dc67430e9380555c8d19fdc
Ricardo Sebastián González Rivas <a01646105@tec.mx>
2026-05-31 01:27:23 -0600

    Actualizar ALUCtrl con mejores comentarios

--- Single Cycle RISC V/Ricardo Sebastian Gonzalez Rivas/ALUCtrl.v
+++ Single Cycle RISC V/Ricardo Sebastian Gonzalez Rivas/ALUCtrl.v
@@ -1,28 +1,33 @@
-// Este modulo representa el ALU en si
+// Este módulo se encarga de las operaciones con la ALU principal
+// utilizando las señales de salida de la Register File
 module ALUCtrl #(parameter N = 12) (
+    // Entradas
     input [N-1:0] A,
     input [N-1:0] B,
-    input [2:0] ALUControl,
-    output reg Zero,
+    input [2:0] ALUControl,     // Señal de control para la ALU
+    
+    // Salidas
+    output reg Zero,            // Señal booleana en caso de instrucción tipo B
     output reg [N-1:0] result
 );
 
+// La operación depende de la señal de control
 always @(*)
 begin
     case(ALUControl)
-        3'b000: result = A + B; // Add
-        3'b001: result = A - B; // Subtract
-        3'b010: result = A & B; // And
-        3'b011: result = A | B; // Or
-        3'b101: // Also A << B // SLT (Set on less than) 
+        3'b000: result = A + B;   // Add
+        3'b001: result = A - B;   // Subtract
+        3'b010: result = A & B;   // And
+        3'b011: result = A | B;   // Or
+        3'b101: // Also A << B    // SLT (Set on less than) 
             begin
                 if (A < B)
                     result = 1;
                 else result = 0;
             end
-        default: result = 0;
     endcase
 
+    // Lógica para señal Zero
     if (result == 0)
         Zero = 1;
     else
