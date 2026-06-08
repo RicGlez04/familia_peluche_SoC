69a67718b1aae435b88b4ad0f5c8d9f97e4a0d03
Ricardo Sebastián González Rivas <a01646105@tec.mx>
2026-06-07 18:12:42 -0600

    Update Adder.v

--- Pipeline RISC V/Adder.v
+++ Pipeline RISC V/Adder.v
@@ -1,11 +1,12 @@
-// Función: Sumador utilizado para calcular las direcciones de salto y el incremento del PC.
+// Función: Sumador utilizado para calcular las direcciones de salto y el incremento del PC
+// dentro de la etapa Exectue.
 module Adder(
 	input [31:0] PC,
-	input [31:0] variable,
-	output reg [31:0] AdderOut
+	input [31:0] variable,      // Inmediato 
+	output reg [31:0] AdderOut  // Suma del inmediato y PC
 );
 
 always@(*)
-	AdderOut=PC+variable;
+	AdderOut=PC+variable;       // Asignación directa
 
 endmodule
