2b8d6f4eeb0c74d90f1a564448f6217907d0ce32
Ricardo Sebastián González Rivas <a01646105@tec.mx>
2026-06-07 18:21:06 -0600

    Refactor BranchComparator module comments and formatting
    Updated comments and formatting for clarity.

--- Pipeline RISC V/BranchComparator.v
+++ Pipeline RISC V/BranchComparator.v
@@ -1,10 +1,10 @@
 // Función: Lógica de control de saltos que determina si el PC debe tomar una nueva dirección.
 module BranchComparator(
-	input Branch, Zero, Jump,
+	input Branch, Zero, Jump,     // Señales binarias
 	output reg PCSrc
 );
 
 always@(*)
-	PCSrc=(Branch & Zero) | Jump;
+	PCSrc=(Branch & Zero) | Jump; // Señal de control en caso de Branch o Jump
 
 endmodule
