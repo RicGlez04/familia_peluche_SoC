096011906053d2640fcc3e4202971db22e0633d5
Ricardo Sebastián González Rivas <a01646105@tec.mx>
2026-06-07 18:16:35 -0600

    Update Alu.v

--- Pipeline RISC V/Alu.v
+++ Pipeline RISC V/Alu.v
@@ -1,9 +1,10 @@
-// Función: Realiza operaciones matemáticas y lógicas.
+// Función: Realiza operaciones matemáticas y lógica. El ALU principal
+// dentro de la etapa Execute.
 module Alu(
-	input [31:0] An, Bn, 
-	input [2:0] AluControl,
-	output reg [31:0] Alu_result,
-	output reg ZeroE
+	input [31:0] An, Bn,           // SrcA, SrcB
+	input [2:0] AluControl,        // Código de control
+	output reg [31:0] Alu_result,  // Resultado de la operación
+	output reg ZeroE			   // Señal binaria para Branch
 );
 
 always @(*)
@@ -17,7 +18,8 @@ always @(*)
 			5: Alu_result=An << Bn[4:0];
 			default: Alu_result=0;
 		endcase
-		
+
+		// Operación Zero en caso de Branch
 		if(Alu_result==0)
 			ZeroE=1;
 		else
