ffbdb086c41e3908b50b7cc471c40bbe3543cb05
Ricardo Sebastián González Rivas <a01646105@tec.mx>
2026-06-07 18:30:00 -0600

    Update InstructionMemory.v

--- Pipeline RISC V/InstructionMemory.v
+++ Pipeline RISC V/InstructionMemory.v
@@ -1,13 +1,13 @@
 // Función: Memoria ROM que almacena el código máquina de las instrucciones del programa.
 module InstructionMemory(
-	input [31:0] A,
-	output reg [31:0] RD
+	input [31:0] A,         // Dirección de la instrucción en memoria
+	output reg [31:0] RD    // Registro destino
 );
 
 reg[31:0] rom [0:63];
 
 initial begin
-    $readmemh("C:/Diseno de Chips/Pipeline/programa.mem", rom);
+	$readmemh("C:/Diseno de Chips/Pipeline/programa.mem", rom); // Cambiar dirección del archivo si es necesario
 end
 
 always@(*)
