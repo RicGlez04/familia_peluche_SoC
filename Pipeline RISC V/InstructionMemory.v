d7a6d715bffc57e611eb2d634976e2d7cb07cbba
Mariana Eileen Martinez Mancilla <marianaemtzman@gmail.com>
2026-06-07 22:47:01 -0600

    Update InstructionMemory to use fixed ROM data
    Replaced $readmemh with fixed data for instruction memory to be able to test with top module.

--- Pipeline RISC V/InstructionMemory.v
+++ Pipeline RISC V/InstructionMemory.v
@@ -6,8 +6,20 @@ module InstructionMemory(
 
 reg[31:0] rom [0:63];
 
+//initial begin
+//	$readmemh("programa.mem", rom); // Cambiar dirección del archivo si es necesario
+//end
+
+// Remplazar el $readmemh por datos fijos para que funcione en el Top
 initial begin
-	$readmemh("C:/Diseno de Chips/Pipeline/programa.mem", rom); // Cambiar dirección del archivo si es necesario
+	rom[0] = 32'h00510113; // Primera instrucción (PC = 0)
+	rom[1] = 32'h00512223; // Segunda instrucción (PC = 4)
+	rom[2] = 32'hFE000CE3; // Tercera instrucción (PC = 8)
+	
+	// Rellenamos algunas posiciones más para que el pipeline no lea 'X' cuando avance el PC
+	rom[3] = 32'h00510113; 
+	rom[4] = 32'h00512223;
+	rom[5] = 32'hFE000CE3;
 end
 
 always@(*)
